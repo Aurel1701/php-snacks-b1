66ef6df156a14a499c89fa8919e4b64be497249c
Aurel1701 <gantzfan96@gmail.com>
2022-05-20 12:42:54 +0200

    create array partite

---
+++ Snack-1/index.php
@@ -0,0 +1,53 @@
+<!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+
+<?php 
+/* creo Array di partite  */
+$partite = [
+    
+    [
+        'casa' => 'Miami',
+        'ospite' => 'Bulls',
+        'scoreCasa' => 45,
+        'scoreOspite' => 50,
+    ],
+
+    [
+        'casa' => 'Armani',
+        'ospite' => 'Leakers',
+        'scoreCasa' => 70,
+        'scoreOspite' => 40,
+    ],
+
+    [
+        'casa' => 'Siena',
+        'ospite' => 'Olimpia',
+        'scoreCasa' => 50,
+        'scoreOspite' => 65,
+    ],
+
+    [
+        'casa' => 'Boston',
+        'ospite' => 'New York ',
+        'scoreCasa' => 55,
+        'scoreOspite' => 89,
+    ],
+    
+];
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1></h1>
+</body>
+</html>
