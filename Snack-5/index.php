234741fda1556d441afed3e30c732989ed58564c
Aurel1701 <gantzfan96@gmail.com>
2022-05-23 11:57:41 +0200

    edit snack 5

--- Snack-5/index.php
+++ Snack-5/index.php
@@ -44,3 +44,31 @@
     ],
 ];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Live-Snack</title>
+</head>
+<body>
+
+
+<?php foreach ($posts as $key => $value) {
+echo "<h3>$key</h3>";
+
+    foreach ($value as $post) {
+        echo $post['title'];
+        echo '<br>';
+        echo $post['author'];
+        echo '<br>';
+        echo $post['text'];
+    }
+}
+?>
+    
+
+</body>
+</html>
