2fac3b14a5a143cd49f91809e018404e4be2cbb7
Aurel1701 <gantzfan96@gmail.com>
2022-05-23 12:39:46 +0200

    edit foreach

--- Snack-7/index.php
+++ Snack-7/index.php
@@ -26,3 +26,38 @@ Stampiamo il nostro array mettendo gli insegnanti
     ]
 ];
 
+$teachers = $db['teachers'];
+$pms = $db['pm'];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .gray {
+            background-color: gray;
+        }
+        .green {
+            background-color: green;
+        }
+    </style>
+</head>
+<body>
+<h1>Risultato:</h1>
+
+<?php foreach ($db as $role => $people) { ?>
+        <div class="<?= $role === 'pm' ? 'green' : 'gray' ?>">
+                <?php foreach ($people as $person) : ?>
+                    <p><?= "$person[name] $person[lastname]" ?></p>
+                <?php endforeach; ?>
+        </div>
+    <?php } ?>
+?>
+    
+</body>
+</html>
